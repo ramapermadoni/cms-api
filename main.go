38578e0d7bc4c34e592b77980eb3c95b3d27b9e9
Rama Permadoni <ramaprogramming@gmail.com>
2024-10-20 22:10:13 +0700

    seeding-revision

--- main.go
+++ main.go
@@ -19,17 +19,19 @@ import (
 
 func main() {
 	// seed := flag.Bool("seed", false, "Run database seeder")
-	seed := viper.GetBool("seed")
 	flag.Parse()
 	config.Initiator()
 	logger.Initiator()
 	connection.InitDB()
 	migration.AutoMigrate()
 	// Jalankan seeding jika flag --seed digunakan
+	seed := viper.GetBool("seed")
 	if seed {
 		fmt.Println("Seeding database...")
 		seeders.SeedAll(connection.DB)
 		return // Hentikan aplikasi setelah seeding
+	} else {
+		fmt.Println("Not seeding database...")
 	}
 	InitiateRouter()
 }
